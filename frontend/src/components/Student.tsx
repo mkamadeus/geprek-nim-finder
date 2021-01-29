e5245990fa75731c65e3540aee73a7d283c50476
Matthew Kevin Amadeus <13518035@std.stei.itb.ac.id>
2021-01-30 01:07:28 +0700

    Pagination added

--- frontend/src/components/Student.tsx
+++ frontend/src/components/Student.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import majorMap from '@/json/list_jurusan.json';
+import facultyMap from '@/json/list_fakultas.json';
 
 interface Props {
   name: string;
@@ -16,15 +17,32 @@ const Student = (props: Props) => {
     return majorMap[majorCode];
   };
 
+  const getFaculty = () => {
+    const facultyCode: keyof typeof facultyMap = props.facultyId.slice(
+      0,
+      3,
+    ) as keyof typeof facultyMap;
+    return facultyMap[facultyCode];
+  };
+
+  const major = getMajor();
+  const faculty = getFaculty();
+
   return (
     <div className="flex justify-between items-center py-2">
       <div>
         <div className="text-sm font-medium">{props.name}</div>
-        <div className="text-xs italic text-gray-500">{getMajor()}</div>
+        <div className="text-xs italic text-gray-500 select-none">
+          {major || faculty}
+        </div>
       </div>
       <div>
-        <div className="text-xs text-gray-500">{props.majorId}</div>
-        <div className="text-xs text-gray-500">{props.facultyId}</div>
+        {major ? (
+          <div className={`text-xs text-gray-500`}>{props.majorId}</div>
+        ) : null}
+        <div className={`text-xs text-gray-500 ${major ? 'select-none' : ''}`}>
+          {props.facultyId}
+        </div>
       </div>
     </div>
   );
--- frontend/src/components/StudentList.tsx
+++ frontend/src/components/StudentList.tsx
@@ -7,21 +7,27 @@ import { IStudent } from '@/models/Student';
 const StudentList = () => {
   const { keywords, chips } = useContext(SearchContext);
   const [result, setResult] = useState<IStudent[]>([]);
+  const [count, setCount] = useState<number>(0);
   const [isLoading, setLoading] = useState<boolean>(false);
 
   const filterData = useCallback(async () => {
     setLoading(true);
     const data = new Promise<IStudent[]>((resolve, _) => {
-      const tmp = studentData.filter((s: IStudent) =>
-        s.n.toLowerCase().includes(keywords),
-      );
+      const tmp = studentData
+        .filter((s: IStudent) => s.n.toLowerCase().includes(keywords))
+        .sort((x, y) => {
+          if (x.n > y.n) return 1;
+          if (x.n < y.n) return -1;
+          return 0;
+        });
       resolve(tmp);
     });
     return data.then((students) => {
       setLoading(false);
+      setCount(10);
       setResult(students);
     });
-  }, [keywords, chips, setLoading, setResult]);
+  }, [keywords, chips, setLoading, setResult, setCount]);
 
   useEffect(() => {
     if (keywords.length >= 3) {
@@ -34,15 +40,29 @@ const StudentList = () => {
   } else {
     return (
       <>
-        {result.map((s) => {
-          console.log(s);
-          return (
-            <>
-              <Student name={s.n} facultyId={s.f} majorId={s.j} />
-              <hr />
-            </>
-          );
-        })}
+        <div className="pb-4">
+          {result.slice(0, Math.min(result.length, count)).map((s) => {
+            // console.log(s);
+            return (
+              <>
+                <Student name={s.n} facultyId={s.f} majorId={s.j} />
+                <hr />
+              </>
+            );
+          })}
+        </div>
+        <div className="flex justify-center w-full">
+          {result.length != count ? (
+            <button
+              className="border border-teal-500 rounded text-teal-500 px-2 p-1"
+              onClick={() => {
+                setCount(count + 10);
+              }}
+            >
+              Load more...
+            </button>
+          ) : null}
+        </div>
       </>
     );
   }
